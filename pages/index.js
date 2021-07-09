e211db72f6646318f31865b0986864a35a6915cf
l0ve2cr3ate <l0ve2cr3ate@gmail.com>
2021-07-09 20:29:57 +0200

    Connect firestore to server. Store sites in db.

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,37 @@
-import { Button, Heading, Text } from "@chakra-ui/react";
+import { Button, Text, Flex } from "@chakra-ui/react";
+import Head from "next/head";
 import { useAuth } from "@/lib/auth";
+import Logo from "@/icons/logo";
 
 export default function Home() {
   const auth = useAuth();
 
   return (
-    <div>
-      <main>
-        <Heading>Fast Feedback</Heading>
+    <Flex
+      as="main"
+      direction="column"
+      align="center"
+      justify="center"
+      h="100vh"
+    >
+      <Head>
+        <title>Fast Feedback</title>
+      </Head>
 
-        <Text>Current user: {auth?.user ? auth?.user?.email : "None"}</Text>
-        {auth?.user ? (
-          <Button onClick={() => auth.signout()}>Sign Out</Button>
-        ) : (
-          <Button onClick={() => auth.signinWithGithub()}>Sign In</Button>
-        )}
-      </main>
-    </div>
+      <Logo color="black" boxSize="64px" />
+
+      {auth?.user ? (
+        <Button onClick={() => auth.signout()}>Sign Out</Button>
+      ) : (
+        <Button
+          mt={4}
+          variant="link"
+          size="sm"
+          onClick={() => auth.signinWithGithub()}
+        >
+          Sign In
+        </Button>
+      )}
+    </Flex>
   );
 }
